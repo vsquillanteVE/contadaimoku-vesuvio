bb63e8aa0229bdac5251588c5918fe1025b4a3db
vsquillanteVE <vsquillanteVE@example.com>
2025-04-16 19:34:46 +0200

    Migliorato il design: aggiunto logo in trasparenza come sfondo, ingrandito il logo, esteso gli elementi fino alla fine della pagina

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src="/logo.png" alt="Logo" className="logo" />
+        <img src="/logo.png" alt="Logo Capitolo Litorale Vesuviano" className="logo-image" />
         <h1>Conta Daimoku Capitolo Litorale Vesuviano</h1>
       </div>
     </header>
